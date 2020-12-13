21d1931858b3833c180c10c188706feea8b4f6cb
stefano longo <kstel30@gmail.com>
2020-12-13 14:22:33 +0100

    update

--- android/app/src/main/kotlin/com/example/Covid_Tracking/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.Covid_Tracking
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/stelk/Covid_Tracking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.stelk.Covid_Tracking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
